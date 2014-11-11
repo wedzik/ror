af122e262b931070836dfaa7b8d665f9cfb4e126
wedzik <wedzikmail@gmail.com>
2014-11-12 01:46:43 +0200

    litle method name changes

--- dz1/task1.rb
+++ dz1/task1.rb
@@ -1,29 +1,29 @@
 class Task1
   def initialize
-    @avTempHash= Array.new(4){ Array.new(4) { Random.rand(-10..10) } }
+    @avTempHash= Array.new(4){ Array.new(4) { Random.rand(-10..10) }}
     @negativeSum = 0;
-    @avTempHash.each{ |m| m.each{ |x| self.mPrint(x) }; puts }
+    @avTempHash.each{ |m| m.each{ |x| self.m_print(x) }; puts }
   end
 
-  def mPrint(x)
-    print ' ' if(x >= 0)
-    print ' ' if(x.abs < 10)
-    print ' '+x.to_s
+  def m_print(x)
+    print ' ' if (x >= 0)
+    print ' ' if (x.abs < 10)
+    print ' ' + x.to_s
   end
 
-  def testNegative(x)
+  def test_negative(x)
     if(x < 0)
       @negativeSum += x
-      print ' '+x.to_s
+      print ' ' + x.to_s
     end
   end
 
-  def getNegativeSum
-    @avTempHash.each{ |m| m.each{ |x| self.testNegative(x) } }
+  def get_negative_sum
+    @avTempHash.each{ |m| m.each{ |x| self.test_negative(x) } }
     puts
     @negativeSum
   end
 end
 
 t = Task1.new
-puts "Negative sum= #{t.getNegativeSum}";
+puts "Negative sum= #{t.get_negative_sum}";
