d6312fe5dfa47abd349034e20e25b2b19cb564c4
wedzik <wedzikmail@gmail.com>
2014-11-12 23:36:28 +0200

    Optimize dictionary search code

--- dictionary_search/dictionary_search.rb
+++ dictionary_search/dictionary_search.rb
@@ -1,29 +1,23 @@
+#require 'benchmark'
 class DictionarySearch
   def initialize(file_path)
     @data = Hash.new
     @word_pairs = Array.new()
-
     lines = File.readlines(file_path)
     lines.each do |line|
       line.strip!
       if line.size > 2 && line[-1] != line[-2]
         key = line.chop.chop!
-        if @data.has_key?(key)
-          @data[key].push(line)
-        else
-          @data[key] = Array(line)
-        end
+        @data[key] ||=  Array.new()
+        @data[key] << line
       end
     end
   end
 
   def search_pair(arr, w)
-    w2 = String.new(w)
-    t = w2[-2]
-    w2[-2] = w2[-1]
-    w2[-1] = t
+    w2 = "#{w[0..-3]}#{w[-1]}#{w[-2]}"
     if arr.include?(w2)
-      @word_pairs << [w,w2] #if !@word_pairs.include?([w,w2])
+      @word_pairs << [w,w2]
       arr.delete(w2)
     end
   end
@@ -38,6 +32,14 @@ class DictionarySearch
   end
 end
 
-# dict = DictionarySearch.new("./wordsEn.txt")
-# pairs =  dict.word_pairs
-# puts "Found #{pairs.count}"
+# Benchmark.bm() do |x|
+#    x.report("Search" ) {
+#       dict = DictionarySearch.new("./wordsEn.txt")
+#       pairs =  dict.word_pairs
+#       puts "Found #{pairs.count}"
+#   }
+# end
+
+
+
+
