2ec5f36a4e5b28e71f8731454a5d2aa85b6ea15b
wedzik <wedzikmail@gmail.com>
2014-11-17 03:10:40 +0200

    add new search method

--- dictionary_search/dictionary_search.rb
+++ dictionary_search/dictionary_search.rb
@@ -1,5 +1,5 @@
-#require 'benchmark'
-class DictionarySearch
+require 'benchmark'
+class DictionarySearch1
   def initialize(file_path)
     @data = Hash.new
     @word_pairs = Array.new()
@@ -7,7 +7,7 @@ class DictionarySearch
     lines.each do |line|
       line.strip!
       if line.size > 2 && line[-1] != line[-2]
-        key = line.chop.chop!
+        key = line[0..-3]
         @data[key] ||=  Array.new()
         @data[key] << line
       end
@@ -32,14 +32,108 @@ class DictionarySearch
   end
 end
 
+class DictionarySearch0
+  def initialize(file_path)
+    @data = Hash.new
+    @word_pairs = Array.new()
+    lines = File.readlines(file_path).uniq
+    lines.each do |w1|
+      w1.strip!
+      if w1.size > 2 && w1[-1] != w1[-2]
+        w2 = get_swiched(w1)
+        if @data.has_key?(w2)
+          @word_pairs << [w1,w2]
+          @data.delete(w2)
+        elsif
+          @data[w1] = w1
+        end
+      end
+    end
+  end
+
+  def get_swiched(w)
+    "#{w[0..-3]}#{w[-1]}#{w[-2]}"
+  end
+
+  def word_pairs
+    return @word_pairs
+  end
+end
+
+class DictionarySearch2
+  def initialize(file_path)
+    @word_pairs = Array.new()
+    @data = Hash[File.read(file_path).split("\n").uniq.map{|v| [v.strip, v.strip]}]
+    @data.each_key do |key1|
+      if key1 && key1.size > 2 && key1[-1] != key1[-2]
+        key2 = get_swiched(key1)
+        if @data.has_key?(key2)
+          @word_pairs << [key1,key2]
+          @data.delete(key2)
+          @data.delete(key1)
+        end
+      end
+    end
+  end
+
+  def get_swiched(w)
+    "#{w[0..-3]}#{w[-1]}#{w[-2]}"
+  end
+
+  def word_pairs
+    return @word_pairs
+  end
+end
+
 # Benchmark.bm() do |x|
-#    x.report("Search" ) {
-#       dict = DictionarySearch.new("./wordsEn.txt")
-#       pairs =  dict.word_pairs
-#       puts "Found #{pairs.count}"
+#   x.report("ruslan_:") {
+#     dict = DictionarySearch0.new("./wordsEn.txt")
+#     pairs =  dict.word_pairs
+#   }
+#
+#   x.report("wedzik1:") {
+#     dict = DictionarySearch1.new("./wordsEn.txt")
+#     pairs =  dict.word_pairs
+#   }
+#
+#   x.report("wedzik2:") {
+#     dict = DictionarySearch2.new("./wordsEn.txt")
+#     pairs =  dict.word_pairs
 #   }
 # end
-
-
+#
+# Benchmark.bm() do |x|
+#   x.report("ruslan_:") {
+#     dict = DictionarySearch0.new("./wordsEn_1ml.txt")
+#     pairs =  dict.word_pairs
+#   }
+#
+#   x.report("wedzik1:") {
+#     dict = DictionarySearch1.new("./wordsEn_1ml.txt")
+#     pairs =  dict.word_pairs
+#   }
+#
+#   x.report("wedzik2:") {
+#     dict = DictionarySearch2.new("./wordsEn_1ml.txt")
+#     pairs =  dict.word_pairs
+#   }
+# end
+#
+# Benchmark.bm() do |x|
+#   x.report("ruslan_:") {
+#     dict = DictionarySearch0.new("./wordsEn_10ml.txt")
+#     pairs =  dict.word_pairs
+#   }
+#
+#   x.report("wedzik1:") {
+#     dict = DictionarySearch1.new("./wordsEn_10ml.txt")
+#     pairs =  dict.word_pairs
+#   }
+#
+#   x.report("wedzik2:") {
+#     dict = DictionarySearch2.new("./wordsEn_10ml.txt")
+#     pairs =  dict.word_pairs
+#   }
+# end
 
 
